dcceb6c6fb9009071b4e7ccc46d6401d4aae9a3a
Daniel Getahun <danielgetahun46@gmail.com>
2025-12-03 08:51:45 -0600

    fixed

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -97,7 +97,8 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
-
+    struct list locks_owned_list;
+    
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
